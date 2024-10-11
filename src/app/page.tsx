a98341da91e7dd0686e67f04fde08593572139ef
gbonic <gbonic00@fesb.hr>
2024-10-11 15:13:42 +0200

    Created a landing page for practice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div style={{fontWeight:900}}><h1>Home Page</h1></div>
+  return (<div><h1 style={{fontWeight:900}}>Home Page</h1></div>
   );
 }
