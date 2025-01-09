828060a2c28199419f93b4215f329942682522a0
ivaarlovic <iva.arlovic03@gmail.com>
2025-01-09 23:04:46 +0100

    omogucit klik na cijeli element a ne samo na tekst

--- assignments/06-assignment/src/app/popular-recipes/page.tsx
+++ assignments/06-assignment/src/app/popular-recipes/page.tsx
@@ -18,28 +18,28 @@ export default function PopularRecipes() {
         <main className="grid grid-rows-[1fr_auto_1fr] min-h-screen">
             <section className="flex flex-wrap justify-center items-center gap-8">
                 {popularRecipes.map((recipe, index) => (
-                    <div
-                        key={index}
-                        className="relative w-72 h-40 rounded-lg overflow-hidden shadow-md"
+                    <Link
+                        href={recipe.path}
+                        className="text-center text-xl font-bold drop-shadow-md text-[#fde4b5] uppercase"
                     >
-                        {/* Slika recepta */}
-                        <img
-                            src={recipe.image}
-                            alt={recipe.title}
-                            className="w-full h-full object-cover"
-                        />
-                        {/* Tekst preko slike */}
-                        <div className="absolute inset-0 flex justify-center items-center text-center backdrop-blur-[3px] bg-black/20">
-                            <div className="bg-black/30 px-4 py-2 w-72">
-                                <Link
-                                    href={recipe.path}
-                                    className="text-center text-xl font-bold drop-shadow-md text-[#fde4b5] uppercase"
-                                >
+                        <div
+                            key={index}
+                            className="relative w-72 h-40 rounded-lg overflow-hidden shadow-md hover:scale-105 transition-transform duration-300"
+                        >
+                            {/* Slika recepta */}
+                            <img
+                                src={recipe.image}
+                                alt={recipe.title}
+                                className="w-full h-full object-cover"
+                            />
+                            {/* Tekst preko slike */}
+                            <div className="absolute inset-0 flex justify-center items-center text-center backdrop-blur-[3px] bg-black/20">
+                                <div className="bg-black/30 px-4 py-2 w-72">
                                     {recipe.title}
-                                </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </section>
         </main>
