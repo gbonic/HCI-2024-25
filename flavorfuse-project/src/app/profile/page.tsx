c453f21f0667eb1310053c9fa7e1538cfc8b5caf
ivaarlovic <iva.arlovic03@gmail.com>
2025-03-08 15:20:28 +0100

    Mobile version

--- flavorfuse-project/src/app/profile/page.tsx
+++ flavorfuse-project/src/app/profile/page.tsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
   ];
 
   return (
-    <div className="max-w-4xl mx-auto p-8 bg-white rounded-xl shadow-lg border border-gray-300 mt-10">
+    <div className="max-w-4xl mx-auto p-4 sm:p-8 bg-white rounded-xl shadow-lg border border-gray-300 mt-10">
       <div className="flex flex-col items-center">
         <div className="w-24 h-24 bg-gray-300 rounded-full flex items-center justify-center">
           <svg
@@ -37,16 +37,16 @@ const ProfilePage = () => {
         <h2 className="mt-4 text-2xl font-bold text-gray-800">{userName}</h2>
       </div>
 
-      <div className="flex justify-around mt-6">
-        <div className="text-center">
+      <div className="flex flex-wrap justify-around mt-6">
+        <div className="text-center mb-4 sm:mb-0">
           <span className="block text-xl font-bold text-gray-800">0</span>
           <span className="text-gray-600">Moji recepti</span>
         </div>
-        <div className="text-center">
+        <div className="text-center mb-4 sm:mb-0">
           <span className="block text-xl font-bold text-gray-800">0</span>
           <span className="text-gray-600">Moji komentari</span>
         </div>
-        <div className="text-center">
+        <div className="text-center mb-4 sm:mb-0">
           <span className="block text-xl font-bold text-gray-800">0</span>
           <span className="text-gray-600">Moje ocjene</span>
         </div>
@@ -56,12 +56,12 @@ const ProfilePage = () => {
         </div>
       </div>
 
-      <div className="mt-6 flex justify-center space-x-4">
+      <div className="mt-6  mb-6 flex flex-wrap justify-center space-x-2 sm:space-x-4">
         {tabs.map((tab) => (
           <button
             key={tab.id}
             onClick={() => setActiveTab(tab.id)}
-            className={`px-4 py-2 rounded-full font-semibold ${
+            className={`px-4 py-2 mb-2 rounded-full font-semibold ${
               activeTab === tab.id
                 ? "bg-orange-500 text-white"
                 : "bg-gray-200 text-gray-800"
@@ -72,7 +72,7 @@ const ProfilePage = () => {
         ))}
       </div>
 
-      <div className="mt-6">
+      <div className="mt-6 mb-6">
         {activeTab === "moji-recepti" && (
           <div>
             <h3 className="text-xl font-bold text-gray-800">Moji recepti</h3>
